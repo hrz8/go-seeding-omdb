6f402266f1105cfcd441e9b8ff7d8cb2d8ed8951
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-10 23:03:54 +0700

    update fetch function

--- domains/movie/utils/utils.go
+++ domains/movie/utils/utils.go
@@ -3,18 +3,28 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 
+	"github.com/hrz8/go-seeding-omdb/helpers"
 	"github.com/hrz8/go-seeding-omdb/models"
 )
 
 type (
 	movie struct {
-		Title  string `json:"Title"`
-		Year   string `json:"Year"`
-		ImdbID string `json:"imdbID"`
-		Type   string `json:"Type"`
-		Poster string `json:"Poster"`
+		Title      string  `json:"Title"`
+		Year       string  `json:"Year"`
+		ImdbID     string  `json:"imdbID"`
+		Type       string  `json:"Type"`
+		Poster     string  `json:"Poster"`
+		Released   *string `json:"released,omitempty"`
+		Runtime    *string `json:"runtime,omitempty"`
+		Director   *string `json:"director,omitempty"`
+		Writer     *string `json:"writer,omitempty"`
+		Actors     *string `json:"actors,omitempty"`
+		Plot       *string `json:"plot,omitempty"`
+		Language   *string `json:"language,omitempty"`
+		Country    *string `json:"country,omitempty"`
+		ImdbRating *string `json:"imdbRating,omitempty"`
+		ImdbVotes  *string `json:"imdbVote,omitempty"`
 	}
 
 	omdbResponse struct {
@@ -28,18 +38,13 @@ func FetchOmdb(apiKey *string, payload *models.MoviePayloadList) (*omdbResponse,
 	URL := fmt.Sprintf("https://www.omdbapi.com/?apikey=%s&s=%s&page=%d",
 		*apiKey, payload.Searchword, payload.Pagination,
 	)
-	var client = &http.Client{}
 	var data omdbResponse
 
-	request, err := http.NewRequest("GET", URL, nil)
+	response, err := helpers.Fetch("GET", URL)
 	if err != nil {
 		return nil, err
 	}
 
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, err
-	}
 	defer response.Body.Close()
 
 	err = json.NewDecoder(response.Body).Decode(&data)
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "net/http"
+
 // NilToEmptyMap is a helper function to convert nil value to {}
 func NilToEmptyMap(d *interface{}) interface{} {
 	data := *d
@@ -7,4 +9,18 @@ func NilToEmptyMap(d *interface{}) interface{} {
 		data = make(map[string]interface{})
 	}
 	return data
+}
+
+// Fetch is a helper function to get the http response
+func Fetch(method string, url string) (*http.Response, error) {
+	var client = &http.Client{}
+	request, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
