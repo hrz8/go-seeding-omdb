b9d722490debbbf0270ca4de97488aa036e01c2f
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-10 22:53:21 +0700

    update movie struct

--- models/movie.go
+++ models/movie.go
@@ -3,11 +3,21 @@ package models
 type (
 	// Movie represents movie object from omdb response
 	Movie struct {
-		Title  string `json:"title"`
-		Year   string `json:"year"`
-		ImdbID string `json:"imdbID"`
-		Type   string `json:"type"`
-		Poster string `json:"poster"`
+		Title      string  `json:"title"`
+		Year       string  `json:"year"`
+		ImdbID     string  `json:"imdbID"`
+		Type       string  `json:"type"`
+		Poster     string  `json:"poster"`
+		Released   *string `json:"released,omitempty"`
+		Runtime    *string `json:"runtime,omitempty"`
+		Director   *string `json:"director,omitempty"`
+		Writer     *string `json:"writer,omitempty"`
+		Actors     *string `json:"actors,omitempty"`
+		Plot       *string `json:"plot,omitempty"`
+		Language   *string `json:"language,omitempty"`
+		Country    *string `json:"country,omitempty"`
+		ImdbRating *string `json:"imdbRating,omitempty"`
+		ImdbVotes  *string `json:"imdbVote,omitempty"`
 	}
 
 	// MoviePayloadList represents query params to fetch all movies
